eabb0aa6b429d59680af7972a387fd8c530c3db4
AbhishekAligh <abhishekaligh@AbhishekAlighs-Mac-mini.local>
2022-02-25 03:04:59 +0530

    create rectangle annotation

--- src/components/WebViewer.vue
+++ src/components/WebViewer.vue
@@ -10,7 +10,15 @@ export default {
     url: String,
   },
   //Initialize the webviewer instance in the mounted lifecycle hook
-
+  data() {
+    return {
+      annotations: [
+        { page: 2, x: 150, y: 150, w: 50, h: 20, id: "1" },
+        { page: 1, x: 80, y: 90, w: 50, h: 20, id: "2" },
+        { page: 1, x: 120, y: 120, w: 50, h: 20, id: "3" },
+      ],
+    };
+  },
   mounted() {
     WebViewer(
       {
@@ -19,7 +27,32 @@ export default {
       },
       this.$refs.viewer
     ).then((instance) => {
-        instance.setTheme("dark");
+      const { docViewer, annotManager, Annotations } = instance;
+      instance.setTheme("dark");
+      instance.disableElements(["searchButton"]);
+      docViewer.setMargin(40);
+
+      docViewer.on("documentLoaded", () => {
+
+        for (let annotId = 0; this.annotations.length; annotId++) {
+          let annot = this.annotations[annotId];
+          annotManager.setCurrentUser("Abhishek Aligh");
+          const rectangleAnnot = new Annotations.RectangleAnnotation();
+          rectangleAnnot.PageNumber = annot.page;
+          rectangleAnnot.X = annot.x;
+          rectangleAnnot.Y = annot.y;
+          rectangleAnnot.Width = annot.w;
+          rectangleAnnot.Height = annot.h;
+          rectangleAnnot.Author = annotManager.getCurrentUser();
+          rectangleAnnot.FillColor = new Annotations.Color(100, 1500, 130, 0.1);
+          rectangleAnnot.StrokeColor = new Annotations.Color(0, 0,0);
+          rectangleAnnot.StrokeThickness = 5;
+          rectangleAnnot.setCustomData("id", annot.id);
+
+          annotManager.addAnnotation(rectangleAnnot);
+          annotManager.redrawAnnotation(rectangleAnnot);
+        }
+      });
     });
   },
 };
